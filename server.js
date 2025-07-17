30efd1c23a77e7aba10d63a258aea0b59b20f185
Jonthss <jonathas9000@gmail.com>
2025-07-17 20:00:19 -0300

    Add error handling middleware and custom error page

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
@@ -12,11 +12,11 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/") // <-- ADICIONADO: Importa o módulo de utilitários
 
 /* ***********************
  * View Engine and Templates
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -26,6 +26,43 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+// This route should catch any requests that didn't match previous routes
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+// This is the LAST middleware to handle any errors that occur
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message
+  let status = err.status || 500 // Default to 500 if no status is provided
+
+  // Customize message for 404
+  if (status == 404) {
+    message = err.message || 'Sorry, we appear to have lost that page.'
+  } else {
+    message = err.message || 'Oh no! There was a crash. Maybe try a different route?'
+  }
+
+  res.render("errors/error", {
+    title: status + ' Error', // Use status in the title
+    message: message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
@@ -40,11 +77,3 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-
-// Index route
-app.get("/", baseController.buildHome)
-app.use("/inv", inventoryRoute)
-
-
-// change test
